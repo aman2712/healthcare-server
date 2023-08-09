244e17912d8fe9b68da5ff77fb9dbbe18f60e4a4
aman2712 <khanamankhan1973@gmail.com>
2023-08-09 20:49:55 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import Data from "./models/dataModel.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // get all data
 app.get("/", async (req, res) => {
